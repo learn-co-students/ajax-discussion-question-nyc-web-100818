aa64056cfbcd8580f40db43d1dfda753c07616f1
tirempbs <tirem@fastmail.com>
2018-11-26 14:40:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+const newPerson = document.querySelector('.btn.btn-primary')
+// User Attributes
+const profilePictureImg = document.querySelector('#profile_picture')
+const fullnameHTML = document.querySelector('#fullname')
+const emailHTML = document.querySelector('#email')
+const streetHTML = document.querySelector('#street')
+const cityHTML = document.querySelector('#city')
+const stateHTML = document.querySelector('#state')
+const postcodeHTML = document.querySelector('#postcode')
+const phoneHTML = document.querySelector('#phone')
+const cellHTML = document.querySelector('#cell')
+const dobHTML = document.querySelector('#date_of_birth')
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+  newPerson.addEventListener('click', function(e) {
+    fetch('https://randomuser.me/api/')
+      .then(resp => resp.json())
+      .then(parsedNewUser => {
+        userInfo = parsedNewUser.results[0]
+        console.log(userInfo)
+
+        profilePictureImg.src = userInfo.picture.large
+        fullnameHTML.innerText = fullname(userInfo.name)
+        emailHTML.innerText = userInfo.email
+        streetHTML.innerText = userInfo.location.street
+        cityHTML.innerText = upperString(userInfo.location.city)
+        stateHTML.innerText = upperString(userInfo.location.state)
+        postcodeHTML.innerText = userInfo.location.postcode
+        phoneHTML.innerText = userInfo.phone
+        cellHTML.innerText = userInfo.cell
+        dobHTML.innerText = userInfo.dob.date
+      })
+  })
+
+  function fullname(name) {
+    return upperString(name.first) + ' ' + upperString(name.last) }
+
+  function upperString(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
 });
